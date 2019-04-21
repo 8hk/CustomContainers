fbe8309f5053bcc16d9c036286238f3bd38ab2da
sibumi <sibumi@sibumi>
2019-04-21 22:43:44 +0300

    LinkedListQueue implementation

---
+++ src/TakeHomeExam/LinkedListQueue.java
@@ -0,0 +1,20 @@
+package TakeHomeExam;
+
+public class LinkedListQueue <T> extends CustomLinkedList<T>{
+
+	public LinkedListQueue(int volume) {
+		super(volume);
+		// TODO Auto-generated constructor stub
+	}
+	void enqueue(T val){
+		insertAfter(0, val);
+	}
+	
+	T dequeue(){
+		int size = size();
+		T temp = get(size);
+		removeAt(size-1);
+		return temp;
+	}
+
+}
